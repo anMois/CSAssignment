6cfcd1770a3926989629e1b4a625bffd2367bae4
anMois <kew1672@naver.com>
2024-07-18 17:09:25 +0900

    HelloWord
    문법의 시작인 HelloWord 작성

---
+++ Day1/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Day1
+{
+    internal class Program
+    {
+        //2024.07.09
+        static void Main(string[] args)
+        {
+            Console.WriteLine("내 이름은 {0}, 나이는 {1}살 {2}다", "박재성", 29, "남자");
+            Console.WriteLine("나는 다음 내용을 배울 \"준비\"되었다");
+        }
+    }
+}
