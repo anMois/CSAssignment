a00ffcfc75153891ab097999c50cc2d07b0821a3
anMois <kew1672@naver.com>
2024-07-22 18:41:28 +0900

    OOP
    객체지향 클래스 240722

---
+++ OOP/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace OOP
+{
+    internal class Program
+    {        
+        class Chracter
+        {
+            public int level;
+            public int hp;
+            public float moveSpeed;
+            public int attack;
+
+            public void MoveFront()
+            {
+                Console.WriteLine("앞으로 이동");
+            }
+
+            public void MoveBack()
+            {
+                Console.WriteLine("뒤로 이동");
+            }
+
+            public void MoveLeft()
+            {
+                Console.WriteLine("왼쪽으로 90도 회전");
+            }
+
+            public void MoveRight()
+            {
+                Console.WriteLine("오른쪽으로 90도 회전");
+            }
+
+            public void Attack(int _atk)
+            {
+                Console.WriteLine($"{_atk} 만큼의 공격을 하였다.");
+            }
+            
+            public void SufferDamage(int _damage)
+            {
+                Console.WriteLine($"{_damage} 만큼의 피해를 받았다.");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
