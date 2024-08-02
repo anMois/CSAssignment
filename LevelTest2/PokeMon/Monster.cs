355d0525cfe36d55c9d13d6d5e974613e7524775
anMois <kew1672@naver.com>
2024-08-02 16:29:37 +0900

    [add] pokemon
    각 포켓몬이 있으며 각각 공격하는 것이 다르다.

---
+++ LevelTest2/PokeMon/Monster.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokeMon
+{
+    public abstract class Monster
+    {
+        public string name;
+
+        public Monster(string _name)
+        {
+            this.name = _name;
+        }
+
+        public abstract void Attack();
+    }
+
+    public class Pikachu : Monster
+    {
+        public Pikachu() : base("피카츄")
+        {
+
+        }
+
+        public Pikachu(string _name) : base(_name)
+        {
+
+        }
+
+        public override void Attack()
+        {
+            Console.WriteLine("백만볼트!");
+        }
+    }
+
+    public class Charmander : Monster
+    {
+        public Charmander() : base("파이리")
+        {
+
+        }
+
+        public Charmander(string _name) : base(_name)
+        {
+
+        }
+        public override void Attack()
+        {
+            Console.WriteLine("화염방사!");
+        }
+    }
+    public class Squirtle : Monster
+    {
+        public Squirtle() : base("꼬북이")
+        {
+
+        }
+
+        public Squirtle(string _name) : base(_name)
+        {
+
+        }
+        public override void Attack()
+        {
+            Console.WriteLine("물총발사!");
+        }
+    }
+
+    public class Bulbasaur : Monster
+    {
+        public Bulbasaur() : base("이상해씨")
+        {
+
+        }
+
+        public Bulbasaur(string _name) : base(_name)
+        {
+
+        }
+        public override void Attack()
+        {
+            Console.WriteLine("덩굴채찍!");
+        }
+    }
+}
---
+++ LevelTest2/PokeMon/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace PokeMon
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Monster[] monsters = new Monster[5];
+            monsters[0] = new Pikachu();
+            monsters[1] = new Charmander();
+            monsters[2] = new Squirtle();
+            monsters[3] = new Bulbasaur();
+            monsters[4] = new Pikachu("털뭉치");
+
+            foreach (Monster monster in monsters)
+            {
+                Console.WriteLine($"{monster.name} 공격해!");
+                monster.Attack();
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
