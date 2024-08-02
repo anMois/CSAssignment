b9e00a35a21b5d51e8135aa84eeec2464676464d
anMois <kew1672@naver.com>
2024-08-02 16:31:58 +0900

    중복 확인
    제시된 리스트에 값을 넣고 연속되는 숫자는 하나만 가질 수 있는 배열을 가짐

---
+++ LevelTest2/CodTest/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+
+namespace CodTest
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> lists = new List<int>();
+
+            lists.Add(1);
+            lists.Add(1);
+            lists.Add(3);
+            lists.Add(3);
+            lists.Add(0);
+            lists.Add(1);
+            lists.Add(1);
+
+
+            foreach (int i in Solution(lists))
+            {
+                Console.Write(i + " ");
+            }
+        }
+
+
+        public static List<int> Solution(List<int> arry)
+        {
+            List<int> answer = new List<int>();
+
+            for (int i = 0; i < arry.Count; i++)
+            {
+                int temp = arry[i];
+                if (i == 0)
+                {
+                    answer.Add(temp);
+                }
+                //answer의 해당 값이 있는 지 확인 후 
+                //arry의 i번째와 그 전꺼를 비교해서 다른지 확인 
+                //answer에 해당 값이 있어도 arry의 순번으로 전의 값과 체크해서
+                //같으면 false고 다르면 true로 처리해서 answer의 값을 추가한다.
+                else if (!answer.Contains(temp) || arry[i] != arry[i - 1])
+                {
+                    answer.Add(temp);
+                }
+            }
+
+            return answer;
+        }
+    }
+}
