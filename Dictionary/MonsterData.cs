3773cf108d1f138de2515712c578ee794eb072f2
anMois <kew1672@naver.com>
2024-08-02 16:38:04 +0900

    Dictionary
    탐색이 유용한 딕셔너리를 이용한 몬스터 데이터에 값을 넣고 몬스터를 소환하면 해당 몬스터 데이터를 가져와서 활용하는 코드

---
+++ Dictionary/MonsterData.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionary
+{
+    public enum JobType { Attacker, Defanser, Supporter, Healer, Specialist }
+    
+    public class MonsterData
+    {
+        public string name;
+        public JobType type;
+        public int hp;
+        public int attak;
+        public int defanse;
+
+        public MonsterData(string _name, JobType _type, int _hp, int _atk, int _def)
+        {
+            this.name = _name;
+            this.type = _type;
+            this.hp = _hp;
+            this.attak = _atk;
+            this.defanse = _def;
+        }
+    }
+
+    public class Monster
+    {
+        public string name;
+        public JobType type;
+        public int hp;
+        public int attak;
+        public int defanse;
+
+        public Monster(MonsterData _data)
+        {
+            this.name = _data.name;
+            this.type = _data.type;
+            this.hp = _data.hp;
+            this.attak = _data.attak;
+            this.defanse = _data.defanse;                
+        }
+    }
+}
---
+++ Dictionary/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Dictionary
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, MonsterData> monsterData = new Dictionary<string, MonsterData>();
+
+            monsterData.Add("가드", new MonsterData("가드", JobType.Attacker, 120, 150, 100));
+            monsterData.Add("디펜더", new MonsterData("디펜더", JobType.Defanser, 150, 80, 200));
+            monsterData.Add("서포터", new MonsterData("서포터", JobType.Supporter, 100, 100, 100));
+            monsterData.Add("힐러", new MonsterData("힐러", JobType.Healer, 100, 70, 80));
+            monsterData.Add("스페셜리스트", new MonsterData("스페셜리스트", JobType.Specialist, 100, 250, 70));
+
+
+            Monster guard = new Monster(monsterData["가드"]);
+
+            PrintMonster(guard);
+        }
+
+        static void PrintMonster(Monster _monster)
+        {
+            Console.WriteLine($"이  름 : {_monster.name}");
+            Console.WriteLine($"타  입 : {_monster.type}");
+            Console.WriteLine($"체  력 : {_monster.hp}");
+            Console.WriteLine($"공격력 : {_monster.attak}");
+            Console.WriteLine($"방어력 : {_monster.defanse}");
+        }
+    }
+}
